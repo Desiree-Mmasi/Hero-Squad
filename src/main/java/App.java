b425774e2f2e03145885a9230d139b1de69ef652
Moringa School <mmasidesiree9@gmail.com>
2019-06-24 10:26:51 +0300

    This commit will change the files that were initally created using Handlebars and now use Velocity Template Engines

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
@@ -11,6 +11,7 @@ public class App {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
+        }, new VelocityTemplateEngine());
+
     }
 }
