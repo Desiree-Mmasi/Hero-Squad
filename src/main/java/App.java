d08f615d42116302b9812eef5ff91ea9dc4daa97
Moringa School <mmasidesiree9@gmail.com>
2019-06-24 16:42:47 +0300

    This commit will add the changes made in the layout.vtl, add the heroform.vtl, and also add the created Java Classes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
@@ -7,10 +8,37 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-
+        String layout = "templates/layout.vtl";
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(new HashMap(), "templates/layout.vtl");
+            return new ModelAndView(new HashMap(), layout);
+        }, new VelocityTemplateEngine());
+
+        post("/hero", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Hero> hero =  request.session().attribute("hero");
+            if (hero==null) {
+                hero= new ArrayList<Hero>();
+                request.session().attribute("hero",hero);
+            }
+
+            String inputtedHero = request.queryParams("hero");
+            String inputtedAge = request.queryParams("age");
+            String inputtedpower = request.queryParams("power");
+            String inputtedweakness = request.queryParams("weakness");
+            Hero herol = new Hero();
+            hero.add(herol);
+            request.session().attribute("hero", hero);
+            model.put("username", hero);
+
+            model.put("template", "templates/success.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/hero/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template","templates/heroform.vtl");
+            return new ModelAndView(model,layout );
         }, new VelocityTemplateEngine());
 
     }
