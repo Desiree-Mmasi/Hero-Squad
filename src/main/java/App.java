15e40488e7fe36d24731c1fbb7ab0f37863107f6
Moringa School <mmasidesiree9@gmail.com>
2019-06-25 12:36:54 +0300

    This commit will add the hero form and also modify the User Interface depending on their respective files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,20 +9,21 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
-        get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(new HashMap(), layout);
-        }, new VelocityTemplateEngine());
 
-        post("/hero", (request, response) -> {
+        get("/" ,(request, response) -> {
+           Map<String,Object> model = new HashMap<String,Object>();
+            model.put("template", "templates/index.vtl");
+           return  new ModelAndView(model, layout);
+        },new VelocityTemplateEngine());
+
+
+        post("/done", (request, respsonse) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             ArrayList<Hero> hero =  request.session().attribute("hero");
             if (hero==null) {
                 hero= new ArrayList<Hero>();
                 request.session().attribute("hero",hero);
-
             }
-
             String inputtedHero = request.queryParams("hero");
             String inputtedAge = request.queryParams("age");
             int age = Integer.parseInt(inputtedAge);
@@ -31,17 +32,23 @@ public class App {
             Hero herol = new Hero(inputtedHero,age,inputtedpower,inputtedweakness);
             hero.add(herol);
             request.session().attribute("hero", hero);
-            model.put("username", hero);
-
+            model.put("hero", hero);
             model.put("template", "templates/success.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
-//        get("/hero/new", (request, response) -> {
-//            Map<String, Object> model = new HashMap<String, Object>();
-//            model.put("template","templates/heroform.vtl");
-//            return new ModelAndView(model,layout );
-//        }, new VelocityTemplateEngine());
 
+        get("/hero", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template","templates/heroform.vtl");
+            return new ModelAndView(model,layout );
+        }, new VelocityTemplateEngine());
+
+
+        get("/view", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template","templates/hero.vtl");
+            return new ModelAndView(model,layout );
+        },new VelocityTemplateEngine());
     }
 }
